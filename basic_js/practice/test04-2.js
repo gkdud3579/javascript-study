94b06c76bd6a45d4829704b49ff41961c60a6524
Hayoung Shim <gkdud3579@gmail.com>
2023-10-25 18:58:43 +0900

    solved test04-2

---
+++ basic_js/practice/test04-2.js
@@ -0,0 +1,24 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = '';
+
+rl.on('line', function (line) {
+    input = line;
+}).on('close', function () {
+    let result = '';
+    for (let i = 0; i < input.length; i++) {
+        let char = input.charAt(i);
+        if (char >= 'a' && char <= 'z') {
+            result += char.toUpperCase();
+        } else if (char >= 'A' && char <= 'Z') {
+            result += char.toLowerCase();
+        } else {
+            result += char;
+        }
+    }
+    console.log(result);
+});
