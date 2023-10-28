8813a35a92a296ebf41db2a690e21e8ab09c1b37
Hayoung Shim <gkdud3579@gmail.com>
2023-10-28 21:47:39 +0900

    solved test19

---
+++ basic_js/practice/test19.js
@@ -0,0 +1,28 @@
+function solution(ineq, eq, n, m) {
+    var answer = 0;
+    if(ineq == "<" && eq == "="){
+        if(n < m || n == m)
+            answer = 1;
+        else
+            answer = 0;
+    }
+    else if(ineq == "<" && eq == "!"){
+        if(n < m && n != m)
+            answer = 1;
+        else
+            answer = 0;
+    }
+    else if(ineq == ">" && eq == "="){
+        if(n > m || n == m)
+            answer = 1;
+        else
+            answer = 0;
+    }
+    else {
+        if(n > m && n != m)
+            answer = 1;
+        else
+            answer = 0;
+    }
+    return answer;
+}
