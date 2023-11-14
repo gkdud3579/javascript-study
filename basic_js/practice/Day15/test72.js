b56634a6ce55166a040f5c571c7096e6b8c8bb7a
Hayoung Shim <gkdud3579@gmail.com>
2023-11-14 21:07:10 +0900

    resolved test72

--- basic_js/practice/Day15/test72.js
+++ basic_js/practice/Day15/test72.js
@@ -1,15 +1,23 @@
 //조건에 맞게 수열 변환하기 2
 
 function solution(arr) {
-    var answer = 0;
-    for(var i = 0; i < arr.length; i++){
-        if(arr[i] >= 50 && arr[i] % 2 === 0)
-            answer = arr[i] / 2;
-        else if(arr[i] < 50 && arr[i] % 2 !== 0)
-            answer = arr[i] * 2 + 1;
-        else
-            answer = arr[i];
-        answer = Math.min(i);
+    let idx = 0
+    let prevArr = arr
+    while(true) {
+        // 현재 배열을 조건에 맞게 변환
+        const changeCurArr = prevArr.map(a => {
+            if(a >= 50 && a%2 === 0) return a/2
+            if(a < 50 && a%2 === 1) return a*2+1
+            return a
+        })
+        // 이전의 모든 배열과 현재 모든 배열의 요소 비교
+        const isAllSame = prevArr.every((a, i) => a === changeCurArr[i])
+        if(isAllSame) break
+        idx+=1
+        
+        // 현재 배열을 이전 배열 변수에 저장함
+        prevArr = changeCurArr
     }
-    return answer;
+    
+    return idx
 }
