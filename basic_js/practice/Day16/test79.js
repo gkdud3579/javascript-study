52de027fb11ba420dccffc62721457511fd3de81
Hayoung Shim <gkdud3579@gmail.com>
2023-11-15 20:21:27 +0900

    resolved test79

--- basic_js/practice/Day16/test79.js
+++ basic_js/practice/Day16/test79.js
@@ -1,12 +1,5 @@
 //A 강조하기
 
-function solution(myString) {
-    var answer = '';
-    for(var i = 0; i < myString.length; i++){
-        if(myString[i] === 'a')
-            answer += myString[i].toUpperCase();
-        else
-            answer += myString[i].toLowerCase();
-    }
-    return answer;
-}
+function solution(myString){
+    return  myString.toLowerCase().replace(/a/g,"A")
+  }
