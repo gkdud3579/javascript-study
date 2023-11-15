0cd6c97d7e8c71d3e3735c863fd31d18f864a27a
Hayoung Shim <gkdud3579@gmail.com>
2023-11-15 21:11:15 +0900

    solved test83

---
+++ basic_js/practice/Day17/test83.js
@@ -0,0 +1,11 @@
+//ad 제거하기
+
+function solution(strArr) {
+    var answer = [];
+    for (var i = 0; i < strArr.length; i++) {
+        if (!strArr[i].includes("ad")) {
+            answer.push(strArr[i]);
+        }
+    }
+    return answer;
+}
