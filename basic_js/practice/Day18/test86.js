231074b2a8de7d5d6bf142ea8fa00be2f838e65c
Hayoung Shim <gkdud3579@gmail.com>
2023-11-16 15:14:26 +0900

    solved test86

---
+++ basic_js/practice/Day18/test86.js
@@ -0,0 +1,12 @@
+//x사이의 개수
+
+function solution(myString) {
+    var answer = [];
+    var parts = myString.split("x");
+
+    for (var i = 0; i < parts.length; i++) {
+        answer.push(parts[i].length);
+    }
+
+    return answer;
+}
