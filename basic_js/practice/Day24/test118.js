2f9d59b42bc282c4af10fd2f61f6cbe00e7267ca
Hayoung Shim <gkdud3579@gmail.com>
2023-11-27 21:36:54 +0900

    solved test118

---
+++ basic_js/practice/Day24/test118.js
@@ -0,0 +1,12 @@
+// 조건에 맞게 수열 변환하기 3
+
+function solution(arr, k) {
+    var answer = [];
+    for(var i = 0; i < arr.length; i++){
+        if(k % 2 !== 0)
+            answer.push(arr[i] * k);
+        else
+            answer.push(arr[i] + k);
+    }
+    return answer;
+}
